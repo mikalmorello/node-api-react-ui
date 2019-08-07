10b25996b2085ef58cb7096574b1596be475c5a2
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-08-06 22:32:23 -0400

    THEME - Add initial styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,11 @@ class Header extends React.Component {
   render(){
     return (
       <header className="header">
-        HEADER
+        <div className="header__container">
+          <a href="https://www.seas.harvard.edu/" className="header__logo">
+            <img src="/assets/media/seas-logo.png" alt="Seas Logo"/>
+          </a>
+        </div>
       </header>
     )
   }
